3e964d492d2b5ec37c2bc4fcd586c65d1f5270ac
AGENT_2405_4\age24 <Y2233020@hamagin.co.jp>
2024-07-09 11:29:35 +0900

    first commit

---
+++ app/ui/FontTsx/FontTsx.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export type FontTsxProps = {
+  // props go here
+};
+export default function FontTsx(props: FontTsxProps) {
+  return (
+    <>
+      <h1>FontTsx</h1>
+    </>
+  );
+}
